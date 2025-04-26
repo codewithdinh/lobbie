abac9714c75314c040554db46834c05814858095
codewithdinh <thiendinhbui208@gmail.com>
2025-04-26 13:04:55 -0500

    Add supabase url

--- src/client.jsx
+++ src/client.jsx
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const URL = import.meta.env.VITE_SUPABASE_URL;
+const URL = 'https://xlyqjujsoiyfikdbfwth.supabase.co';
 const ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(URL, ANON_KEY);
