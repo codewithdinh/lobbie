3fb99b4f38ef299e1eddf0b3428e9f67ad82ef7f
codewithdinh <thiendinhbui208@gmail.com>
2025-04-29 00:07:03 -0500

    Fixed some spacing between sort button, add link to title Lobbie

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,7 +20,7 @@ const Header = () => {
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="brand-icon">
                         <path d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                     </svg>
-                    <h1>Lobbie</h1>
+                    <Link to="/"><h1>Lobbie</h1></Link>
                 </div>
                 
                 <div className="navbar-nav">
